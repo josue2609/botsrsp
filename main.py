0cb4ed9a765e23b12aa2f11c2ccba63864384038
josue2609 <josuetouv@gmail.com>
2021-07-07 11:24:27 +0300

    add requete

--- main.py
+++ main.py
@@ -8,26 +8,11 @@ req = Requete()
 
 @app.route('/')
 def botsrsp():
-    rows = req.getNature()
+    rows = req.getNatureDescripition(1)
     resp = jsonify(rows)
 
     return resp
 
-# @app.route('/nature')
-# def nature():
-#     try:
-#         conn = mysql.connect()
-#         cursor = conn.cursor(pymysql.cursors.DictCursor)
-#         cursor.execute("SELECT * FROM nature")
-#         rows = cursor.fetchall()
-#         resp = jsonify(rows)
-#         resp.status_code = 200
-#         return resp
-#     except Exception as e:
-#         print(e)
-#     finally:
-#         cursor.close()
-#         conn.close()
 
 @app.errorhandler(404)
 def not_found(error=None):
--- requete.py
+++ requete.py
@@ -9,16 +9,81 @@ class Requete:
         self.cursor = self.conn.cursor(pymysql.cursors.DictCursor)
         
 
-    def getNature(self):
+    def getMenu(self):
         req = '''
-                SELECT * FROM nature
+                SELECT MENU FROM menu
                 '''
         self.cursor.execute(req)
         return self.cursor.fetchall()
 
-    # def getNaturePiece(self, natureID):
-    #     req = '''
-    #             SELECT 
-    #             '''
+    def getNatureMenu(self, IDMenu):
+        req = '''
+                   SELECT n.TYPE_NATURE 
+                   FROM appartenir a, nature n, menu m
+                        WHERE a.ID_NATURE = n.ID_NATURE
+                            AND a.ID_MENU = %s 
+	                        AND m.ID_MENU = %s
+                 '''
+        self.cursor.execute(req, (IDMenu,IDMenu))
+        return self.cursor.fetchall()
+    
+    def getNatureID(self, Typenature):
+        req = '''
+                SELECT n.ID_NATURE FROM nature n WHERE TYPE_NATURE = %s
+                '''
+        self.cursor.execute(req, (Typenature))
+        return self.cursor.fetchone()
+
+    def getNaturePiece(self, IDNature):
+        req = '''
+                SELECT DISTINCT p.PIECE, p.NOMBRE_PIECE 
+                FROM contenir c, piece p, nature n, menu m, appartenir a
+                    WHERE c.ID_NATUREC = %s
+                        AND n.ID_NATURE = %s
+                        AND c.ID_PIECEC = p.ID_PIECE
+                '''
+        self.cursor.execute(req, (IDNature,IDNature))
+        return self.cursor.fetchall( )
+    
+    def getNatureCondition(self, IDNature):
+        req = '''
+                SELECT DISTINCT c.CONDITIONS
+                FROM posseder p, nature n, conditions c, appartenir a
+                    WHERE p.ID_NATURE = %s
+                        AND n.ID_NATURE = %s
+                        AND p.ID_CONDITIONS = c.ID_CONDITIONS
+                '''
+        self.cursor.execute(req, (IDNature,IDNature))
+        return self.cursor.fetchall( )
+    
+    def getNatureDescripition(self, IDNature):
+        req = '''
+                SELECT n.DESCRIPTION 
+                FROM nature n 
+                    WHERE ID_NATURE = %s
+                '''
+        self.cursor.execute(req, (IDNature))
+        return self.cursor.fetchall( )
+
+    def getNatureDurée(self, IDNature):
+        req = '''
+                SELECT n.DUREE_TRAITEMENT
+                FROM nature n
+                    WHERE ID_NATURE = %s
+                '''
+        self.cursor.execute(req, (IDNature))
+        return self.cursor.fetchall()
+
+    def getNatureBeneficiaire(self, IDNature):
+        req = '''
+                SELECT DISTINCT c.BENEFICIAIRE
+                FROM beneficier b, nature n, beneficiaire c
+                    WHERE b.ID_NATURE = %s
+                        AND n.ID_NATURE = %s
+                        AND b.ID_BENEFICIAIRE = c.ID_BENEFICIAIRE
+                '''
+        self.cursor.execute(req, (IDNature, IDNature))
+        return self.cursor.fetchall()
+
     def _close(self):
         self.conn.close()
